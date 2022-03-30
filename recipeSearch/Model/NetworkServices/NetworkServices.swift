4c45220204f090aff3376ee3b2629321617b6e7e
ahmedAlameir <99804450+ahmedAlameir@users.noreply.github.com>
2022-03-30 07:02:26 -0700

    network commit

---
+++ recipeSearch/Model/NetworkServices/NetworkServices.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkServices.swift
+//  recipeSearch
+//
+//  Created by ahmed osama on 3/30/22.
+//  Copyright © 2022 ahmed osama. All rights reserved.
+//
+
+import Foundation
+class NetworkServices {
+    let baseURL = "https://api.edamam.com/api/recipes/v2?type=public&q=c&app_id=05d81b04&app_key=b179f1aa9fdb0d7e411ec4a96653d02a"
+
+    
+    func fetchRecipe(query:String,complation:@escaping(Result<Response?,Error>)->Void) {
+        guard let url = URL(string: baseURL.replacingOccurrences(of: "type=public&q=", with: "type=public&q="+query))else{
+            return
+        }
+        let request = URLRequest(url: url)
+                  let task = URLSession.shared.dataTask(with: request) { (data, _, erorr) in
+                      guard let data = data else{
+                          return
+                  }
+                      do{
+                          let result = try JSONDecoder().decode(Response.self, from: data)
+                        complation(.success(result))
+                      }catch{
+                          print("products ")
+                      }
+                  
+              }
+                  task.resume()
+
+    }
+}
---
+++ recipeSearch/view/RecipeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTableViewCell.swift
+//  recipeSearch
+//
+//  Created by ahmed osama on 3/30/22.
+//  Copyright © 2022 ahmed osama. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
